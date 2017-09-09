8791c10c2f600e20f53264c0499d080eb883c8b8
jpbskt <jpbskt@hotmail.com>
2017-09-09 16:41:58 -0300

    Cliente tcp e servidor tcp funcionando

--- client.c
+++ client.c
@@ -1,60 +1,118 @@
-#include<stdio.h> //printf
-#include<string.h> //strlen
-#include<sys/socket.h> //socket
-#include<arpa/inet.h> //inet_addr
+#include <stdio.h> //printf
+#include <string.h> //strlen
+#include <sys/socket.h> //socket
+#include <arpa/inet.h> //inet_addr
  
 int main(int argc , char *argv[])
 {
- int sock;
- struct sockaddr_in server;
- int  server_reply[10];
- int number[10],i,temp;
- //Create socket
- sock = socket(AF_INET , SOCK_STREAM , 0);
- if (sock == -1)
- {
- printf("Could not create socket");
- }
- puts("Socket created");
+	int sock;
+	struct sockaddr_in server;
+	char server_reply[80];
+	int number[10],i, protocol, aux = 1;
+	char ip[16];
+	char temp[10];
+
+	while(aux){
+		printf("Escolha qual protocolo usar: (1 = TCP , 2 = UDP)\n");
+		scanf("%d", &protocol);
+		
+		if ( protocol != 1 && protocol != 2){
+			printf("Escolha de protocolo inválida, por favor tente de novo.\n");
+		} // fim if
+		else {aux = 0;}
+
+	} // fim while
+
+	printf("Escolha um endereço de ip: \n");
+	scanf("%s", ip);
  
- server.sin_addr.s_addr = inet_addr("127.0.0.1");
- server.sin_family = AF_INET;
- server.sin_port = htons( 8880 );
- 
- //Connect to remote server
- if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
- {
- perror("connect failed. Error");
- return 1;
- }
- 
- puts("Connected\n");
- 
- printf("enter 10 number");
- for (i=0;i<10;i++)
- {
- scanf("%d",&temp);
- number[i]= temp;
- }
- 
- if( send(sock , &number, 10*sizeof(int) , 0) < 0)
- {
- puts("Send failed");
- return 1;
- }
- 
- //Receive a reply from the server
- if( recv(sock , &server_reply , 10*sizeof(int), 0) < 0)
- {
- puts("recv failed");
- return;
- }
- 
- puts("Server reply :\n");
- for(i=0;i<10;i++)
- {
- printf("%d\n",server_reply[i]);
- }
- close(sock);
- return 0;
+	if( protocol == 1){ // Se o protocolo escolhido é TCP
+
+		printf("Escreva a mensagem a ser enviada:\n");
+		//gets(temp);
+		scanf("%s", temp);
+
+		//Create socket TCP
+		sock = socket(AF_INET , SOCK_STREAM , IPPROTO_TCP);
+		if (sock == -1) {
+		 	printf("Could not create socket");
+		 } // fim if
+
+		puts("Socket created");
+
+		server.sin_addr.s_addr = inet_addr(ip);
+		server.sin_family = AF_INET;
+		server.sin_port = htons( 8880 );
+		 
+		//Connect to remote server
+		if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0) {
+			 perror("connect failed. Error");
+			 return 1;
+		 } // fim if
+		 
+		puts("Connected\n");
+		 
+		 
+		if( send(sock , &temp, 10*sizeof(int) , 0) < 0) {
+			puts("Send failed");
+			return 1;
+		}
+		 
+		 //Receive a reply from the server
+		if( recv(sock , &server_reply , 10*sizeof(int), 0) < 0) {
+			puts("recv failed");
+			return;
+		}
+		 
+		puts("Server reply :\n");
+		puts(server_reply);
+	} // fim if de TCP
+	else if( protocol == 2){ // Se o protocolo escolhido é UDP
+
+		printf("Escreva a mensagem a ser enviada:\n");
+		scanf("%s", temp);
+
+		if( strcmp(temp, "ping") == 0) {
+			printf("pong\n");
+		} // fim if
+
+		//Create socket UDP
+		sock = socket(AF_INET , SOCK_DGRAM , IPPROTO_UDP);
+		if (sock == -1) {
+		 	printf("Could not create socket");
+		 } // fim if
+
+		puts("Socket created");
+
+		server.sin_addr.s_addr = inet_addr(ip);
+		server.sin_family = AF_INET;
+		server.sin_port = htons( 8880 );
+		 
+		//Connect to remote server
+		//if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0) {
+		//	 perror("connect failed. Error");
+		//	 return 1;
+		//} // fim if
+		 
+		//puts("Connected\n");
+		 
+		 
+		if( sendto(sock , &temp, 10*sizeof(int) , 0, (struct sockaddr *) &server, sizeof(server) ) < 0) {
+			puts("Send failed");
+			return 1;
+		}
+		 
+		 //Receive a reply from the server
+		if( recv(sock , &server_reply , 10*sizeof(int), 0) < 0) {
+			puts("recv failed");
+			return;
+		}
+		 
+		puts("Server reply :\n");
+		puts(server_reply);
+	} // fim else if de UDP
+	 
+	close(sock);
+	
+	return 0;
 }
--- server.c
+++ server.c
@@ -1,96 +1,121 @@
-#include<stdio.h>
-#include<string.h> //strlen
-#include<sys/socket.h>
-#include<arpa/inet.h> //inet_addr
-#include<unistd.h> //write
+#include <stdio.h>
+#include <string.h> //strlen
+#include <sys/socket.h>
+#include <arpa/inet.h> //inet_addr
+#include <unistd.h> //write
  
 int main(int argc , char *argv[])
 {
- int socket_desc , client_sock , c , read_size;
- struct sockaddr_in server , client;
- int message[10],i;
- 
- //Create socket
- socket_desc = socket(AF_INET , SOCK_STREAM , 0);
- if (socket_desc == -1)
- {
- printf("Could not create socket");
- }
- puts("Socket created");
- 
- //Prepare the sockaddr_in structure
- server.sin_family = AF_INET;
- server.sin_addr.s_addr = INADDR_ANY;
- server.sin_port = htons( 8880 );
- 
- //Bind
- if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
- {
- //print the error message
- perror("bind failed. Error");
- return 1;
- }
- puts("bind done");
- 
- //Listen
- listen(socket_desc , 3);
- 
- //Accept and incoming connection
- puts("Waiting for incoming connections...");
- c = sizeof(struct sockaddr_in);
- 
- //accept connection from an incoming client
- client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
- if (client_sock < 0)
- {
- perror("accept failed");
- return 1;
- }
- puts("Connection accepted");
- 
- //Receive a message from client
- while( (read_size = recv(client_sock ,&message , 10*sizeof(int), 0)) > 0 )
- {
- // print client msg at server side
- puts("The number sent by client is: ");
- for(i=0;i<10;i++)
- {
- printf("%d",message[i]);
- }
- bubble_sort(message, 10);
- 
- write(client_sock , &message, 10*sizeof(int));
- 
- }
- 
- if(read_size == 0)
- {
- puts("Client disconnected");
- }
- else if(read_size == -1)
- {
- perror("recv failed");
- }
- 
- return 0;
-}
- 
-void bubble_sort(int list[], int n)
-{
- int c, d, t;
- 
- for (c = 0 ; c < ( n - 1 ); c++)
- {
- for (d = 0 ; d < n - c - 1; d++)
- {
- if (list[d] > list[d+1])
- {
- /* Swapping */
- 
- t = list[d];
- list[d] = list[d+1];
- list[d+1] = t;
- }
- }
- }
-}
+	int socket_desc_TCP, socket_desc_UDP , client_sock , c , read_size;
+	struct sockaddr_in server , client;
+	//int message[10];
+	char message[80] = { 0 };
+	char response[80] = { 0 };
+	int i, flag = 0;
+	 
+	//Create socket TCP
+	socket_desc_TCP = socket(AF_INET , SOCK_STREAM , 0);
+	if (socket_desc_TCP == -1) {
+		printf("Could not create TCP socket");
+	} // fim if
+
+	puts("Socket TCP created");
+
+	// create socket UDP
+	// socket_desc_UDP = socket(AF_INET , SOCK_DGRAM , 0);
+	// if (socket_desc_UDP == -1) {
+	// 	printf("Could not create UDP socket");
+	// } // fim if
+
+	// puts("Socket UDP created");
+
+	//Prepare the sockaddr_in structure
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons( 8880 );
+	 
+	//Bind TCP
+	if( bind(socket_desc_TCP,(struct sockaddr *)&server , sizeof(server)) < 0) {
+		//print the error message
+		perror("bind TCP failed. Error");
+		return 1;
+	} // fim if
+
+	puts("bind TCP done");
+
+	// bind UDP
+	// if ( bind(socket_desc_UDP,(struct sockaddr *)&server, sizeof(server)) < 0) {
+	//  	// print the error message
+	//   	perror("bind UDP failed. Error");
+	//   	return 1;
+	// }
+	
+	// puts("bind UDP done");
+
+	//Listen
+	listen(socket_desc_TCP , 5);
+	 
+	//Accept and incoming connection
+	puts("Waiting for incoming connections...");
+	c = sizeof(struct sockaddr_in);
+	 
+	//accept connection from an incoming client
+	client_sock = accept(socket_desc_TCP, (struct sockaddr *)&client, (socklen_t*)&c);
+	if (client_sock < 0) {
+		perror("accept failed");
+		return 1;
+	} // fim if
+
+	puts("Connection accepted");
+	 
+	//Receive a message from client
+	while( (read_size = recv(client_sock , &message , 10*sizeof(int), 0)) > 0 ) {
+		// print client msg at server side
+		puts("The string sent by client is: ");
+		puts(message);
+
+		if( (strcmp(message, "ping") == 0 || strcmp(message, "Ping") == 0)) {
+		  	response[0] =  'p' ;
+		  	response[1] =  'o' ;
+		  	response[2] =  'n' ;
+		  	response[3] =  'g' ;
+		  	flag = 1;
+		} // fim if
+
+		if(flag == 0 && (strcmp(message, "marco") == 0 ||  strcmp(message, "Marco") == 0)) {
+		  	response[0] =  'p' ;
+		  	response[1] =  'o' ;
+		  	response[2] =  'l' ;
+		  	response[3] =  'o' ;
+		  	flag = 1;
+		} // fim if
+
+		if( flag == 0 && (strcmp(message, "marco") != 0 ||  strcmp(message, "Marco") != 0 || strcmp(message, "ping") != 0 || strcmp(message, "Ping") != 0)) {
+		  	  	response[0]  =  'B' ;
+		  		response[1]  =  'a' ;
+		  		response[2]  =  'd' ;
+		  		response[3]  =  ' ' ;
+		  		response[4]  =  'r' ;
+		  		response[5]  =  'e' ;
+		  		response[6]  =  'q' ;
+		  		response[7]  =  'u' ;
+		  		response[8]  =  'e' ;
+		  		response[9]  =  's' ;
+		  		response[10] =  't' ;
+		} // fim if
+
+		puts(response);
+		 
+		write(client_sock , &response, 10*sizeof(int));
+		 
+	} // fim while
+	 
+	if(read_size == 0) {
+		puts("\nClient disconnected");
+	} // fim if
+	else if(read_size == -1) {
+	 	perror("recv failed");
+	} // fim else if
+	 
+	return 0;
+} // fim função main
